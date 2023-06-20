9b8e0e460618d6e30646b07f8e6343bd91c4be5b
Vinay Sayimpu <vinaysayimpu@gmail.com>
2023-06-20 11:54:13 +0530

    Day two of insta-clone commit -01

--- insta-clone/src/App.js
+++ insta-clone/src/App.js
@@ -5,7 +5,7 @@ import LoginPage from './Components/LoginPage/LoginPage';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world<br></br>Speed 1Thz, memory 1ZB</h1>
+      
       <LoginPage/>
     </div>
   );
--- insta-clone/src/Components/LoginPage/LoginPage.js
+++ insta-clone/src/Components/LoginPage/LoginPage.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import Grid from '@mui/material/Grid'; // Grid version 1
+import inst_image from '../../images/9364675fb26a.svg';
+import insta_logo from '../../images/logoinsta.png';
+import './LoginPage.css';
 
 class LoginPage extends Component {
     constructor(props) {
@@ -11,13 +14,29 @@ class LoginPage extends Component {
             <div>
                 <Grid container>
                     <Grid item xs={3}>
-                        Hello
+
                     </Grid>
                     <Grid item xs={6}>
-                        From
-                    </Grid>
-                    <Grid item xs={3}>
-                        Simplilearn
+                        <div className="loginpage__main">
+                            <div>
+                                <img src={inst_image} width="350" height="500" alt="Instagram_Banner" />
+                            </div>
+                            <div className="loginpage__rightcomponent">
+                                <img className="loginpage__logo" src={insta_logo} width="150" alt="Instagram_Logo" />
+                                <div className="TextEntries">
+                                    <div className="textInput">
+                                        <input className="textUser" type="text" placeholder="Phone number, username or Email" />
+                                        <input className="textPassword" type="password" placeholder="Password" />
+                                        <button className="LoginButton">Log in</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <Grid item xs={3}>
+
+                        </Grid>
+
                     </Grid>
                 </Grid>
             </div>
