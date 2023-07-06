d77e712a06a2c145fc5e7476ced85b9d81db8715
Vinay Sayimpu <vinaysayimpu@gmail.com>
2023-07-06 11:29:04 +0530

    HomePage created

---
+++ insta-clone/src/Components/HomePage/HomePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import './HomePage.css';
+
+class HomePage extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return(
+            <div>Hello world, I'm chitti. Speed 1Thz, Memory 1 ZB</div>
+        )
+    }
+}
+
+export default HomePage
